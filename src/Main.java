8e4fc013a62733f9ebf76a7eea5619d520219a84
difangfang <difangfang@wanda.cn>
2016-10-19 16:25:09 +0800

    增加冒泡排序，快速排序

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,14 @@ public class Main {
         PrintUtil.printArray(arr2);
         PrintUtil.printArray(Sort.shellSort(arr2));
 
+        //冒泡排序
+        int[] arr3 = {1, 4, 13, 2, 3, 4, 0, 99, -2, 7};
+        PrintUtil.printArray(arr3);
+        PrintUtil.printArray(Sort.bubbleSort(arr3));
 
+        //快速排序
+        int[] arr4 = {1, 4, 13, 2, 3, 4, 0, 99, -2, 7};
+        PrintUtil.printArray(arr4);
+        PrintUtil.printArray(Sort.quickSort(arr4));
     }
 }
--- src/Sort.java
+++ src/Sort.java
@@ -61,4 +61,73 @@ public class Sort {
         }
         return arr;
     }
+
+    /**
+     * 冒泡排序
+     *
+     * @param arr
+     * @return
+     */
+    public static int[] bubbleSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return arr;
+        }
+
+        int tmp = 0;
+        for (int i = arr.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    /**
+     * 快速排序
+     *
+     * @param arr
+     * @return
+     */
+    public static int[] quickSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return arr;
+        }
+        return quick(arr, 0, arr.length - 1);
+    }
+
+    private static int[] quick(int[] arr, int start, int end) {
+        if (start >= end) {
+            return arr;
+        }
+        int mid = adjust(arr, start, end);
+        quick(arr, start, mid - 1);
+        quick(arr, mid + 1, end);
+        return arr;
+    }
+
+    private static int adjust(int[] arr, int start, int end) {
+        int x = arr[start];
+        while (start < end) {
+            while (start < end && arr[end] > x) {
+                end--;
+            }
+            if (start < end) {
+                arr[start] = arr[end];
+                start++;
+            }
+            while (start < end && arr[start] < x) {
+                start++;
+            }
+            if (start < end) {
+                arr[end] = arr[start];
+                end--;
+            }
+        }
+        arr[start] = x;
+        return start;
+    }
 }
